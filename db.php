c95a8d9b83412cf2515128ade4e28c6bd51884ec
CodeNestle <153824093+CodeNestle@users.noreply.github.com>
2025-07-16 11:05:25 +0530

    Update db.php

--- db.php
+++ db.php
@@ -1,10 +1,13 @@
 <?php
-$host = "localhost";
-$user = "root";
-$pass = "";
-$dbname = "levelup";
+$host = 'bwuk6hfdeaip73fnxjhm-mysql.services.clever-cloud.com';
+$port = 3306;
+$username = 'unhrmm83fk9v6apd';
+$password = 'q1XwTflllYth6mQFrdiG';
+$database = 'bwuk6hfdeaip73fnxjhm';
 
-$conn = new mysqli($host, $user, $pass, $dbname);
+$conn = new mysqli($host, $username, $password, $database, $port);
+
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
+?>
